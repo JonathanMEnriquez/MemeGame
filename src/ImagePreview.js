0d6f334cd81cbee4dc891903bf24807eebca6f96
user <user@users-MacBook-Pro.local>
2020-03-18 19:35:23 -0700

    made the imagepreview sizing uniform

--- src/ImagePreview.js
+++ src/ImagePreview.js
@@ -19,14 +19,14 @@ const ImagePreview = (props) => {
     }
 
     return (
-        <div className='image-preview' onMouseEnter={showOptions} onMouseLeave={hideOptions}>
+        <div style={{backgroundImage: `url(${src})`}} className={isLive ? 'image-preview': 'image-preview no-actions'}
+            onMouseEnter={showOptions}onMouseLeave={hideOptions} title={alt}>
             <div className="image-preview-body">
                 {options &&
                 <div className="image-options">
                     <img src={Delete} alt='x' onClick={deleteImg} />
                 </div>
                 }
-                <img src={src} alt={alt} className={isLive ? 'image' : 'image no-actions'} />
             </div>
         </div>
     )
